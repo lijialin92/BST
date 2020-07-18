f79ebb92814fbe508b3a121e065f0a2005370202
lijialin990 <lijialin990@lijialindeMacBook-Pro.local>
2020-07-18 20:10:40 +0200

    recursive exercise

--- main.cpp
+++ main.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include "BST.h"
 
+
+
 int main() {
-    BST* root = nullptr;
+
+
+    /*BST* root = nullptr;
     root = root->insert(root, 15);
     root->insert(root, 10);
     root->insert(root, 20);
     root->insert(root, 25);
-    std::cout << root->search(root, 15);
+    std::cout << root->search(root, 15);*/
     return 0;
 }
+
+
+
+
+
+/*
+int f(int n)
+{
+    if (n == 1)
+        return n;
+    else
+        return f(n-1) * n;
+}
+
+int f2(int n){
+    if (n == 1 )
+        return n;
+    if (n == 0)
+        return n;
+    else
+        return f2(n-1) + f2(n-2);
+}*/
